40494d961cca91aedf09a4b0d02a67ed7495f7c5
Akshat Garg <gargaks@sheridancollege.ca>
2024-03-27 09:03:20 -0400

    "Buildins commant v.2"

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
